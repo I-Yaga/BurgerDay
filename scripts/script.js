8823c3193db533feee8aba376d04795a18d38b13
Ilya <yagafov08@gmail.com>
2025-04-22 19:50:01 +0400

    add modal reg

--- scripts/script.js
+++ scripts/script.js
@@ -59,4 +59,48 @@ let themeButton = document.querySelector('.theme-button');
 
 themeButton.onclick = function() {
   page.classList.toggle('dark-theme');
+  page.classList.toggle('light-theme');
 };
+
+// Модальное окно регистрации
+
+// Получаем элементы
+const modal = document.getElementById('myModal');
+const openBtn = document.querySelector('.open-modal-btn');
+const closeBtn = document.querySelector('.close-btn');
+
+// Открытие модального окна
+openBtn.addEventListener('click', () => {
+  modal.style.display = 'flex';
+  setTimeout(() => {
+    modal.classList.add('active');
+  }, 10); // Небольшая задержка для срабатывания transition
+});
+
+// Закрытие модального окна по клику на крестик
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove('active');
+  setTimeout(() => {
+    modal.style.display = 'none';
+  }, 300); // Задержка равна длительности анимации
+});
+
+// Закрытие модального окна по клику вне контента
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.classList.remove('active');
+    setTimeout(() => {
+      modal.style.display = 'none';
+    }, 300); // Задержка равна длительности анимации
+  }
+});
+
+// Закрытие модального окна по нажатию клавиши Esc
+window.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    modal.classList.remove('active');
+    setTimeout(() => {
+      modal.style.display = 'none';
+    }, 300); // Задержка равна длительности анимации
+  }
+});
